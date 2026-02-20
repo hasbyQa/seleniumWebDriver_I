128d0ab5c0d4534590d4cf3e3f77537e753772d9
hasby-shanessa <umutoniwabohasby@gmail.com>
2026-02-20 16:10:13 +0200

    Added Screenshot util

--- src/test/java/com/hasby/newsletter/utils/ScreenshotUtil.java
+++ src/test/java/com/hasby/newsletter/utils/ScreenshotUtil.java
@@ -1,4 +1,17 @@
 package com.hasby.newsletter.utils;
 
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.ByteArrayInputStream;
+
 public class ScreenshotUtil {
+    public static void takeScreenshot(WebDriver driver, String name) {
+        if (driver instanceof TakesScreenshot) {
+            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            Allure.addAttachment(name, "image/png", new ByteArrayInputStream(screenshot), ".png");
+        }
+    }
 }
